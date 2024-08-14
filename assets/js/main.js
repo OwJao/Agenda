8e4d2896acd4a4cf2289c3b0aac73d10c442aefd
João Antônio Assunção <j.antonioassuncao@outlook.com>
2024-08-14 00:08:43 -0300

    Update Tarefas Concluído

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,13 +3,27 @@ function adicionar() {
     if (tarefa.value != "") {
     let item = document.createElement('p', 'pointer')
     let lista = document.getElementById('lista')
+    let riscado = false
     item.innerHTML = `— ${tarefa.value}.`
     lista.appendChild(item)
     tarefa.value = ""
     item.setAttribute('id', 'pointer')
     item.style.width = ('fit-content')
+    item.title = 'Clique uma vez para riscar ou duas vezes para excluir!'
     item.addEventListener('click', function() {
-        item.style.textDecoration = 'line-through'
+        if (riscado == false) {
+            item.style.textDecoration = 'line-through'
+            riscado = true
+            item.title = 'Clique uma vez para retirar o risco ou duas vezes para excluir!'
+        } else {
+            item.style.textDecoration = 'none'
+            riscado = false
+            item.title = 'Clique uma vez para riscar ou duas vezes para excluir!'
+        }
+        
+    })
+    item.addEventListener('dblclick', function() {
+        lista.removeChild(item)
     })
     } else {
         alert('[ERRO] Digite um nome para a tarefa!')
