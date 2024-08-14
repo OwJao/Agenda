302d5acd3b922b6abb4095e42b511b24bd9abaf6
João Antônio Assunção <j.antonioassuncao@outlook.com>
2024-08-14 00:14:43 -0300

    Update Tarefas Salvas

--- assets/js/main.js
+++ assets/js/main.js
@@ -25,8 +25,32 @@ function adicionar() {
     item.addEventListener('dblclick', function() {
         lista.removeChild(item)
     })
+    salvarTarefas();
     } else {
         alert('[ERRO] Digite um nome para a tarefa!')
     }
 }
 
+//Precisei do chat-gpt para utilizar o localStorage.
+function salvarTarefas() {
+    let lista = document.getElementById('lista');
+    let tarefas = [];
+
+    lista.childNodes.forEach(function(item) {
+        tarefas.push(item.innerText);
+    });
+
+    localStorage.setItem('tarefas', JSON.stringify(tarefas));
+}
+
+function carregarTarefas() {
+    let tarefas = JSON.parse(localStorage.getItem('tarefas')) || [];
+    
+    let lista = document.getElementById('lista');
+    tarefas.forEach(function(tarefa) {
+        let item = document.createElement('p');
+        item.innerText = tarefa;
+        lista.appendChild(item);
+    });
+}
+window.onload = carregarTarefas;
